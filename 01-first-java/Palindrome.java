dff20ff9cbd2ca78d85d554d82e8e00e90f4e1fa
Akshada <autiakshada2002@gmail.com>
2022-12-27 13:02:49 +0530

    added new folders

--- 01-first-java/Palindrome.java
+++ 01-first-java/Palindrome.java
@@ -1,7 +1,18 @@
 //To find out whether the given String is Palindrome or not.
-import java.util.Scanner;
-public class Palindrome{
-    public static void main(String[] args) {
-        
+public class Palindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        } 
+
+        int reverse = 0, copy = x;
+
+        while (copy > 0) {
+            int digit = copy % 10;
+            reverse = reverse * 10 + digit;
+            copy /= 10;
+        }
+
+        return reverse == x;
     }
 }
